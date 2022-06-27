46021cb29fac37605395efcad7caad0f4ff13103
Sam Mangham <mangham@gmail.com>
2022-06-27 23:16:18 +0100

    Switched to just using Gunicorn, insecure but whatever

--- surveyjs/surveyjs/urls.py
+++ surveyjs/surveyjs/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", include("service.urls")),
     path("api/", include("service.api")),
     path('admin/', admin.site.urls),
-]
+] + staticfiles_urlpatterns()
